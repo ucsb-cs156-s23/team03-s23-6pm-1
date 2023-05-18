5b7d9e723428c8640050a5b7ed20bdcba237dc5b
Max Fletcher <mfletcher@umail.ucsb.edu>
2023-05-18 14:33:58 -0700

    mf - Add mock calls to index tests to fix tests (for real this time!!!)

--- frontend/src/tests/pages/Restaurants/RestaurantIndexPage.test.js
+++ frontend/src/tests/pages/Restaurants/RestaurantIndexPage.test.js
@@ -4,6 +4,12 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import mockConsole from "jest-mock-console";
 
+// for mocking /api/currentUser and /api/systemInfo
+import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
 const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
@@ -40,6 +46,10 @@ jest.mock('main/utils/restaurantUtils', () => {
 
 
 describe("RestaurantIndexPage tests", () => {
+    // mock /api/currentUser and /api/systemInfo
+    const axiosMock =new AxiosMockAdapter(axios);
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
     const queryClient = new QueryClient();
     test("renders without crashing", () => {
