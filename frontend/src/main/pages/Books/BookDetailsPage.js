85cc89a8700aec1bc4689228152f59176dffdcd1
Max Fletcher <mfletcher@umail.ucsb.edu>
2023-05-20 13:58:37 -0700

    mf: don't test internal caching of ReactQuery

--- frontend/src/main/pages/Books/BookDetailsPage.js
+++ frontend/src/main/pages/Books/BookDetailsPage.js
@@ -11,6 +11,7 @@ export default function BookDetailsPage() {
 
   const { data: book, error: _error, status: _status } =
     useBackend(
+      // Stryker disable next-line all : don't test internal caching of ReactQuery
       [`/api/books?id=${id}`],
       {
         // Stryker disable next-line all : don't test internal caching of React Query
