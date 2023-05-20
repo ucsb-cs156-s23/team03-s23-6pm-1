08fc613c07e60ad8ec95bbacdc79230979a471db
Joe Z <77076276+JoeIsNoJoe@users.noreply.github.com>
2023-05-19 23:38:52 -0700

    Update AppNavbar.js

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -81,6 +81,34 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
             <Nav className="mr-auto">
               {
                 hasRole(currentUser, "ROLE_USER") && (
+                  <NavDropdown title="Hotels" id="appnavbar-hotels-dropdown" data-testid="appnavbar-hotels-dropdown">
+                    <NavDropdown.Item href="/hotels" data-testid="appnavbar-hotels-list">List</NavDropdown.Item>
+                    {hasRole(currentUser, "ROLE_ADMIN") && 
+                    (<NavDropdown.Item href="/hotels/create" data-testid="appnavbar-hotels-create">Create</NavDropdown.Item>)
+                    }
+                  </NavDropdown>
+                )
+              }
+            </Nav>
+
+            <Nav className="mr-auto">
+              {
+                hasRole(currentUser, "ROLE_USER") && (
+                  <NavDropdown title="Restaurants" id="appnavbar-restaurants-dropdown" data-testid="appnavbar-restaurants-dropdown" >
+                    <NavDropdown.Item href="/restaurants" data-testid="appnavbar-restaurants-list">List</NavDropdown.Item>
+                    {
+                      hasRole(currentUser, "ROLE_ADMIN") && (
+                        <NavDropdown.Item href="/restaurants/create" data-testid="appnavbar-restaurants-create">Create</NavDropdown.Item>
+                      )
+                    }
+                  </NavDropdown>
+                )
+              }
+            </Nav>
+
+            <Nav className="mr-auto">
+              {
+                hasRole(currentUser, "ROLE_USER") && (
                   <NavDropdown title="Books" id="appnavbar-books-dropdown" data-testid="appnavbar-books-dropdown" >
                     <NavDropdown.Item href="/books/list" data-testid="appnavbar-books-list">List</NavDropdown.Item>
                     {
